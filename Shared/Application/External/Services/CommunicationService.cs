12ef709caee045ed78b44a18ad9132778b3dfbdd
dcancho <d.cancho.c@gmail.com>
2024-10-19 15:04:28 -0500

    hotfix: update email status check

--- Shared/Application/External/Services/CommunicationService.cs
+++ Shared/Application/External/Services/CommunicationService.cs
@@ -17,20 +17,40 @@ public class CommunicationService
 	}
 
 	public async Task SendEmail(string to, string subject, string body)
-{
-    try
-    {
-        EmailSendOperation emailSendOperation =  await _emailClient.SendAsync(
-            WaitUntil.Completed,
-            senderAddress: "DoNotReply@f130d3f5-1b6d-4fdf-b2c5-b0cc0dfc0734.azurecomm.net",
-            recipientAddress: to,
-            subject: subject,
-            htmlContent: body
-        );	
-    }
-    catch (Exception ex)
-    {
-        throw new ExposableException("Couldn't send access recovery email.", 500, ex);
-    }
-}
+	{
+		try
+		{
+			EmailSendOperation emailSendOperation = await _emailClient.SendAsync(
+				WaitUntil.Completed,
+				senderAddress: "DoNotReply@f130d3f5-1b6d-4fdf-b2c5-b0cc0dfc0734.azurecomm.net",
+				recipientAddress: to,
+				subject: subject,
+				htmlContent: body
+			);
+		}
+		catch (Exception ex)
+		{
+			throw new ExposableException("Couldn't send access recovery email.", 500, ex);
+		}
+	}
+
+	public async Task<bool> IsEmailConnectionOk()
+	{
+		try
+		{
+			await _emailClient.SendAsync(
+				WaitUntil.Started,
+				senderAddress: "DoNotReply@f130d3f5-1b6d-4fdf-b2c5-b0cc0dfc0734.azurecomm.net",
+				recipientAddress: "",
+				subject: "Connection test",
+				htmlContent: "This is a test email to check the connection executed at " + DateTime.Now
+			);
+			return true;
+		}
+		catch
+		{
+			return false;
+		}
+	}
+	
 }
