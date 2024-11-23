b22f210bd8a87c3d327e73acfa67dc68e0c987ef
miikuru002 <jamutaqortegavelez@gmail.com>
2024-11-23 18:31:24 -0500

    update: dtos

--- Operational/Domain/Queries/CollectibleSearchQuery.cs
+++ Operational/Domain/Queries/CollectibleSearchQuery.cs
@@ -2,10 +2,14 @@ namespace Collectioneer.API.Operational.Domain.Queries
 {
 	public record CollectibleSearchQuery(
 		string SearchTerm,
-		int CommunityId = 0
+		int CommunityId = 0,
+		int Page = 1,
+		int PageSize = 10
 	)
 	{
 		public string SearchTerm { get; init; } = SearchTerm;
 		public int CommunityId { get; init; } = CommunityId;
+		public int Page { get; init; } = Page;
+		public int PageSize { get; init; } = PageSize;
 	}
 }
--- Social/Domain/Queries/CommunitySearchContentQuery.cs
+++ Social/Domain/Queries/CommunitySearchContentQuery.cs
@@ -2,9 +2,13 @@ namespace Collectioneer.API.Social.Domain.Queries
 {
 	public record CommunitySearchContentQuery(
 		string SearchTerm,
-		int CommunityId
+		int CommunityId,
+		int Page = 1,
+		int PageSize = 10
 	) {
 		public string SearchTerm { get; init; } = SearchTerm;
 		public int CommunityId { get; init; } = CommunityId;
+		public int Page { get; init; } = Page;
+		public int PageSize { get; init; } = PageSize;
 	}
 }
